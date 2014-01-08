746be30cc6a825efa1024e354f9c81ec209415e8
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-01-08 22:16:35 +0100

    Cask under own user

--- providers/cask.rb
+++ providers/cask.rb
@@ -1,3 +1,7 @@
+extend(Homebrew::Mixin)
+
+owner = homebrew_owner
+
 def load_current_resource
   @cask = Chef::Resource::HomebrewCask.new(new_resource.name)
   cask_dir = @cask.name
@@ -13,6 +17,7 @@ end
 action :cask do
   unless @cask.casked
     execute "installing cask #{new_resource.name}" do
+      user owner
       command "/usr/local/bin/brew cask install #{new_resource.name}"
       not_if "/usr/local/bin/brew brew list | grep #{new_resource.name}"
     end
@@ -22,6 +27,7 @@ end
 action :uncask do
   if @cask.casked
     execute "uninstalling cask #{new_resource.name}" do
+      user owner
       command "/usr/local/bin/brew cask uninstall #{new_resource.name}"
       only_if "/usr/local/bin/brew cask list | grep #{new_resource.name}"
     end
