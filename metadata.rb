ab6bdb5cc8e5385b0667eb64a86482518b33df25
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-10-07 07:43:23 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'roderik.van.der.veer@kunstmaan.be'
 license          'Apache 2.0 + MIT'
 description      'Install Homebrew and use it as your package provider in Mac OS X. An alternative implementation because the default Opscode cookbook did not work as needed for use in Kitchenplan.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.8.1'
+version          '1.8.8'
 recipe           'homebrewalt', 'Install Homebrew and Homebrew Cask'
 supports         'mac_os_x'
 supports         'mac_os_x_server'
